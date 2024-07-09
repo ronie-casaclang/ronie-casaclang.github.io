1fec777b91a311e9deade40b4a6a028884aad685
ronie-casaclang <casaclangronie05@gmail.com>
2024-07-09 21:40:38 +0800

    Work &  About page front-end is completed

--- script/link.js
+++ script/link.js
@@ -2,3 +2,13 @@
 document.getElementById('facebook').href = "https://www.facebook.com/ronie.casaclang";
 document.getElementById('linkedin').href = "https://www.linkedin.com/in/ronie-casaclang";
 document.getElementById('github').href = "https://github.com/ronie-casaclang";
+
+// navigate pages
+document.getElementById('nav-work').href = "../src/work.html";
+document.getElementById('nav-about').href = "../src/about.html";
+document.getElementById('nav-contact').href = "../src/contact.html";
+
+// menus
+document.getElementById('menu-work').href = "../src/work.html";
+document.getElementById('menu-about').href = "../src/about.html";
+document.getElementById('menu-contact').href = "../src/contact.html";
---
+++ script/menu.js
@@ -0,0 +1,4 @@
+// Hide or Display menu
+document.querySelector("#btn-menu").addEventListener("click", function(){
+    document.querySelector(".div-menu").style.display = "flex";
+});
